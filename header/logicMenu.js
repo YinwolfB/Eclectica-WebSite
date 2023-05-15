853eaead91cf6a63b9bafc9d18b7204f25a48c61
RazielBlaster <jhojanblackout@gmail.com>
2023-05-15 13:03:15 -0500

    add menu desktop and folders

---
+++ header/logicMenu.js
@@ -0,0 +1,36 @@
+
+function names() {
+    const btnMenuOpen = document.querySelector(".header__button")
+    const btnMenuClose = document.querySelector(".header__close-btn")
+    
+    
+    const containerMenu = document.querySelector(".header__ul")
+    
+    
+    
+    btnMenuOpen.addEventListener("click", () => {
+        containerMenu.classList.add("header__ul-show")
+    })
+    
+    
+    btnMenuClose.addEventListener("click", () => {
+        containerMenu.classList.remove("header__ul-show")
+    })
+    
+    
+}
+
+
+
+    fetch("../header/headerMenu.html")
+    .then (response => response.text())
+    .then(html => {
+        document.querySelector(".header__menu").innerHTML = html
+        names()
+    }) 
+
+
+
+
+
+
